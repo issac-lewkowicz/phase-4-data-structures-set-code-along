9bc7312909ac59d4aca7b8954438825375af19ba
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-07-07 15:35:11 -0600

    commit tree 0997c48553825e463003eacc490e55e3b8e46008

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,28 @@
 # your code here
+class MySet
+  def initialize(array = [])
+    @hash = {}
+    array.each do |value|
+      @hash[value] = true
+    end
+  end
+
+  def include?(value)
+    !!@hash[value]
+  end
+
+  def add(value)
+    @hash[value] = true
+    self
+  end
+
+  def delete(value)
+    @hash.delete(value)
+    self
+  end
+
+  def size
+    @hash.length
+  end
+
+end
